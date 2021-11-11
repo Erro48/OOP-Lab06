ba81aa5b69b332b46f9ee4f1369aab969d82de61
Enrico <errotagliaferri.4801@gmail.com>
2021-11-11 21:46:48 +0100

    Remove unused import assertTrue and assertFalse

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -1,11 +1,9 @@
 package it.unibo.oop.lab.exception1;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
 
-import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
