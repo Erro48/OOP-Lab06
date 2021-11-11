da83f43bca7c379f46879510f91e331f73258130
Enrico <errotagliaferri.4801@gmail.com>
2021-11-11 21:24:33 +0100

    Add javadoc comments

--- src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -1,5 +1,10 @@
 package it.unibo.oop.lab.exception1;
 
+/**
+ * Represents an exception occurring when battery level of a robot
+ * is not enough.
+ * 
+ */
 public class NotEnoughBatteryException extends IllegalStateException {
 
 	/**
@@ -8,10 +13,21 @@ public class NotEnoughBatteryException extends IllegalStateException {
 	private static final long serialVersionUID = 1L;
 	private final double batteryLevel;
 	
+	/**
+	 * Construct new instance of exception
+	 * 
+	 * @param batteryLevel
+	 * 	            	represent the battery level that caused the exception
+	 * 
+	 */
 	public NotEnoughBatteryException(final double batteryLevel) {
 		this.batteryLevel = batteryLevel;
 	}
 
+	/**
+     * 
+     * @return the string representation of instances of this class
+     */
 	@Override
 	public String toString() {
 		return "Remain battery = " + this.batteryLevel;
