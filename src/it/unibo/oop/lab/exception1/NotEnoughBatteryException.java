b67ea3d7f653790bc1b4a500ca95fb12826f84a1
Enrico <errotagliaferri.4801@gmail.com>
2021-11-11 21:21:40 +0100

    Add serialVersionUID

--- src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -2,6 +2,10 @@ package it.unibo.oop.lab.exception1;
 
 public class NotEnoughBatteryException extends IllegalStateException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private final double batteryLevel;
 	
 	public NotEnoughBatteryException(final double batteryLevel) {
