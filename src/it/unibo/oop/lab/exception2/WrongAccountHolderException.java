a63297e753c6815a93ef34b8e473d3f206bd5fdb
Enrico <errotagliaferri.4801@gmail.com>
2021-11-12 17:33:18 +0100

    Add constructor and toString

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -6,5 +6,18 @@ public class WrongAccountHolderException extends RuntimeException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private final int usrId;
+	
+	public WrongAccountHolderException(final int userID) {
+		this.usrId = userID;
+	}
+	
+	@Override
+	public String toString() {
+		return "User with id(" + this.usrId + ") has no authorization to operate here";
+	}
 
+	public String getMessage() {
+		return this.toString();
+	}
 }
