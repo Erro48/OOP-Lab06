113ba168f21da99c41efe16e6aef72f579cf4d4a
Enrico <errotagliaferri.4801@gmail.com>
2021-11-13 16:31:10 +0100

    Add constructor, toString and getMessage to NotEnoughFoundsException

--- src/it/unibo/oop/lab/exception2/NotEnoughFoundException.java
+++ src/it/unibo/oop/lab/exception2/NotEnoughFoundException.java
@@ -1,5 +1,22 @@
 package it.unibo.oop.lab.exception2;
 
-public class NotEnoughFoundException extends Exception {
+public class NotEnoughFoundException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final double amount;
+	
+	public NotEnoughFoundException(final double amount) {
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Remain money: " + this.amount;
+	}
 
+	public String getMessage() {
+		return this.toString();
+	}
 }
